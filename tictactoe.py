9ebfa6bc874946535538a3ad76f9e5a73b9ef4a7
Hamila Mailee <45606899+hamilamailee@users.noreply.github.com>
2022-05-03 13:08:30 +0430

    TicTacToe exercise started

---
+++ tictactoe.py
@@ -0,0 +1,17 @@
+from mimetypes import init
+import socket
+
+
+class GameServer:
+    def __init__(self) -> None:
+        pass
+
+
+class WebServer:
+    def __init__(self) -> None:
+        pass
+
+
+class Client:
+    def __init__(self) -> None:
+        pass
